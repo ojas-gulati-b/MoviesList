063f4f841933e682b358c61471a1ed87c4562368
ojasgulati-hpe <ojas.gulati@hpe.com>
2021-10-01 15:17:30 +0530

    Added loader changes

---
+++ src/components/Common/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import loaderGif from '../../../assets/images/Loader.gif';
+import './Loader.css';
+
+const Loader = () => {
+    return (<div className='loader'><img src={loaderGif} /></div> )
+}
+
+export default Loader;
